6ce9bfd3840519af1acd071ec2cbcd8127af631f
AlexanderElkin1969 <eaa@mfcmmr.ru>
2022-12-25 00:01:04 +0300

    task1 commit

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,21 @@ public class Main {
 
     public static void task1() {
         System.out.println("Задача 1");
+        byte typeByte = 100;
+        short typeShort = 1000;
+        int typeInt = 1000000;
+        long typeLong = 1000000000L;
+        float typeFloat;
+        double typeDouble;
+        typeFloat = 1f/ 3;
+        typeDouble = 1d/ 3;
+
+        System.out.println("Значение переменной typeByte с типом byte  равно "+typeByte);
+        System.out.println("Значение переменной typeShort с типом short равно "+typeShort);
+        System.out.println("Значение переменной typeInt с типом int равно "+typeInt);
+        System.out.println("Значение переменной typeLong с типом long равно "+typeLong);
+        System.out.println("Значение переменной typeFloat с типом float равно "+typeFloat);
+        System.out.println("Значение переменной typeDouble с типом double равно "+typeDouble);
     }
     public static void task2() {
         System.out.println("Задача 2");
